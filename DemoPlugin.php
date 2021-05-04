fcd9da79ad24535d4840f9a51cc367a70799b054
Fitsum A <fitsumcs@gmail.com>
2021-05-04 21:31:24 +0300

    starting with new demo plugin

---
+++ DemoPlugin.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+* Plugin Name: Demo Plugin 
+* Plugin URI: http://demoplugin.com
+* Description: This is a demo plugin developed for teaching purpose 
+* Version: 1.0.0
+* Author: Fitsum A.
+* Author URI: http://fitsum.com
+* License: GPL2
+*/
+
+?>
